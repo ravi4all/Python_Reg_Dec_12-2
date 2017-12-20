be9ae6717f3d9cd085669afa3dcf983fb22f13d2
ravi4all <ravikanttyagi.2295@gmail.com>
2017-12-20 18:04:49 +0530

    Add files via upload

---
+++ GameDevelopment/05-MovingObjects.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+
+red = (255,150,100)
+black = (0,0,0)
+
+screen = pygame.display.set_mode((900,600))
+
+rect_x = 0
+rect_y = 0
+
+clock = pygame.time.Clock()
+FPS = 50
+
+while True:
+
+    for event in pygame.event.get():
+        # print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    screen.fill(red)
+
+    pygame.draw.rect(screen, black, [rect_x,rect_y,50,50])
+
+    rect_x += 5
+    rect_y += 5
+
+    pygame.display.update()
+    clock.tick(FPS)
---
+++ GameDevelopment/07-InsertImage.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+
+pygame.init()
+
+width = 900
+height = 600
+
+screen = pygame.display.set_mode((width, height))
+
+img_1 = pygame.image.load("ball.jpg")
+
+circle_x = random.randint(60,width-60)
+circle_y = random.randint(60,height-60)
+
+clock = pygame.time.Clock()
+FPS = 150
+
+speed = [2,2]
+
+while True:
+
+    for event in pygame.event.get():
+        # print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    screen.fill((255,255,255))
+
+    screen.blit(img_1, (circle_x, circle_y))
+
+    circle_x += speed[0]
+    circle_y += speed[1]
+
+    if circle_x > width - 200 or circle_x < 0:
+        speed[0] = -speed[0]
+    elif circle_y > height - 200 or circle_y < 0:
+        speed[1] = -speed[1]
+
+    pygame.display.update()
+    clock.tick(FPS)
